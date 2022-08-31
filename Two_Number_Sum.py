97540995722d2efc704fbaf1bbae7ece04f50506
Wenbin Guo <guowenbin90@gmail.com>
2022-08-31 11:11:43 -0400

    Update Two_Number_Sum.py

--- Two_Number_Sum.py
+++ Two_Number_Sum.py
@@ -1,3 +1,35 @@
+# Solution 1 starts
+# O(n^2) time | O(1) space
+def twoNumberSum(array, targetSum):
+    result_list = []
+    for i in range(0, len(array) - 1):
+        first_number = array[i]
+        for j in range(i + 1, len(array)):
+            second_number = array[j]
+            sum = first_number + second_number
+            if sum == targetSum:
+                result_list.append(first_number)
+                result_list.append(second_number)
+
+    return sorted(result_list)
+# Solution 1 ends
+
+# Solution 2 starts
+# O(n) time | O(n) space
+def twoNumberSum2(array, targetSum):
+    result_list = []
+    for i in range(0, len(array) - 1):
+        first_number = array[i]
+        second_nsumber_expected = targetSum - first_number
+        secondary_array = array[i + 1:len(array)]
+        if second_nsumber_expected in secondary_array:
+            result_list.append(first_number)
+            result_list.append(second_nsumber_expected)
+
+    return sorted(result_list)
+# Solution 2 ends
+
+# Solution 3 starts
 # O(nlogn) time | O(1) space
 def twoNumberSum3(array, targetSum):
     array.sort()
@@ -13,5 +45,5 @@ def twoNumberSum3(array, targetSum):
             right -= 1
 
     return []
-
+# Solution 3 ends
 print(twoNumberSum([5, 3, -4, 8, 11, 1, -1, 6], 10))
